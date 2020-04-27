74ed09b638884ab45332a7c8023219d54868974e
Greg Lim <limjunqu@gmail.com>
2020-04-27 14:40:30 +0800

    Update GitHubUser.js

--- src/GitHubUser.js
+++ src/GitHubUser.js
@@ -4,20 +4,24 @@ import { Button } from 'react-bootstrap';
 class GitHubUser extends Component {
  
   constructor(props){ 
-      super(props);  
-      this.handleClick = this.handleClick.bind(this);                                   
+      super(props);    
+      this.handleClick = this.handleClick.bind(this);                                     
   }
 
   handleClick(e) {
     this.props.history.push("/github");  
   }
 
+
   render() { 
     return (
       <div>                
         <h1>User Login: { this.props.match.params.login }</h1>
-        <h2>User Score: { this.props.match.params.score }</h2>`                                                                                                                                
-        <Button bsStyle="primary" onClick={this.handleClick}>Go to GitHub Users</Button>        
+        <h2>User Id: { this.props.match.params.id }</h2>`                                                                                                                                        
+        <Button variant="primary" onClick={this.handleClick}>
+            Go to GitHub Users
+ 	    </Button>      
+
       </div>
     );
   }
