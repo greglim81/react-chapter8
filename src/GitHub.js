398078bddbc23f2b7e82c59c7dfd48378244cf67
Greg Lim <limjunqu@gmail.com>
2020-04-27 14:41:05 +0800

    Update GitHub.js

--- src/GitHub.js
+++ src/GitHub.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios'; // npm install axios
+import axios from 'axios'; 
 import ReactLoading from 'react-loading';
-import { Media, Form, FormGroup, FormControl, Button } from 'react-bootstrap';
-import {Link} from 'react-router-dom'; //
+import { Media, Form, Button, Nav } from 'react-bootstrap';
 
 class GitHub extends Component {    
     
@@ -12,74 +11,88 @@ class GitHub extends Component {
             data: [],
             searchTerm:'',
             isLoading : false            
-        };     
+
+        }; 
+
         this.handleChange = this.handleChange.bind(this);  
-        this.handleSubmit = this.handleSubmit.bind(this);                               
+        this.handleSubmit = this.handleSubmit.bind(this);  
+
     }
 
-    handleChange(e) {
-        this.setState({ searchTerm: e.target.value });        
-    }
+    componentDidMount(){
+        //this.getGitHubData('greg');
+    }	
 
+
+    getGitHubData(_searchTerm){                
+        axios.get("https://api.github.com/search/users?q="+_searchTerm)
+            .then(res => {   
+                this.setState({
+                    isLoading : false, 
+                    data: res.data.items                                  
+                })  
+                             
+                console.log(res.data.items); 
+            });             
+    }          
+
+    render() { 
+        const listUsers = this.state.data.map(user =>             
+            <Media key={user.id}>
+                <Nav.Link href={`/github/user/${user.login}/${user.id}`}> 
+                    <img
+                        width={64}
+                        height={64}
+                        className="mr-3"
+                        src={user.avatar_url}
+                        alt="Generic placeholder"
+                    />
+                </Nav.Link>
+                <Media.Body>
+                    <h5>Login: {user.login}</h5>
+                    <p>Id: { user.id }</p>
+                </Media.Body>
+            </Media>                     
+        );        
+
+        return (
+          <div>
+            <Form inline onSubmit={this.handleSubmit}>
+                <Form.Group controlId="formInlineName">                                    
+                    <Form.Control 
+                        type="text" 
+                        value={this.state.searchTerm} 
+                        placeholder="Enter Search Term" 
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>                            
+                {' '}
+                <Button type="submit">
+                    Search
+                </Button>
+            </Form>                 
+
+            <h3>GitHub Users Results</h3>
+            { this.state.isLoading &&                                             
+                <ReactLoading type="spinningBubbles" color="#444" /> 
+            }  
+            {listUsers}                                                                                            
+          </div>
+        );
+    }
+    
     handleSubmit(e) {
         e.preventDefault();    
         this.setState({
             isLoading : true          
         })                
         this.getGitHubData(this.state.searchTerm);        
-    }      
+    }
 
-    getGitHubData(_searchTerm){                
-        axios.get("https://api.github.com/search/users?q="+_searchTerm)
-            .then(res => {                                   
-                this.setState({
-                    isLoading : false,  
-                    data: res.data.items                 
-                })                            
-                console.log(res.data.items); 
-            });             
-    }          
+    handleChange(e) {
+        this.setState({ searchTerm: e.target.value });        
+    }
 
-    render() {  
-        const listUsers = this.state.data.map((user) =>             
-            <Media key={user.id}>
-            <Media.Left>                
-                <Link to={`/github/user/${user.login}/${user.score}`}>
-                    <img width={64} height={64} src={user.avatar_url} alt="Image"/>
-                </Link>
-            </Media.Left>
-            <Media.Body>
-                <Media.Heading>{user.login}</Media.Heading>
-                <p>Score: { user.score }</p>
-            </Media.Body>
-            </Media>          
-        );        
-        
-        return (
-          <div> 
-            <br />  
-            <Form inline onSubmit={this.handleSubmit}>
-                <FormGroup controlId="formInlineName">                                    
-                    <FormControl 
-                        type="text" 
-                        value={this.state.searchTerm} 
-                        placeholder="Enter Search Term" 
-                        onChange={this.handleChange}
-                    />
-                </FormGroup>                            
-                {' '}
-                <Button type="submit">
-                    Search
-                </Button>
-            </Form>                 
-            <h3>GitHub Users Results</h3>
-            { this.state.isLoading &&                                             
-                <ReactLoading type="spinningBubbles" color="#444" /> 
-            }                               
-            {listUsers}                                                                                            
-          </div>
-        );
-    }       
+    
 }
 export default GitHub;
-
